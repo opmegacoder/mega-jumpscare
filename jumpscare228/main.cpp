0227d11fb692ee982ede880abd6c2a9ca3d2c17d
opmegacoder <86320449+opmegacoder@users.noreply.github.com>
2023-08-06 23:28:00 +0300

    Add files via upload

---
+++ jumpscare228/main.cpp
@@ -0,0 +1,56 @@
+﻿#include "Windows.h"
+#include <iostream>
+
+using namespace std;
+
+void pizdec()
+{
+	int gavno = 2773;
+	for (int i = 0; i < gavno; i++)
+	{
+		SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, (LPARAM)2);
+	}
+
+}
+
+int randomizator(int *udacha)
+{
+	srand(time(NULL));
+
+	*udacha = rand() % 2 + 1;
+
+	return 0;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "RU");
+
+	printf("здаров ты попал на телепорограмму недохакеросы здеся будет решатся судьба твоево монитора\n");
+
+	int bebra = 0;
+
+	Sleep(5000);
+
+	printf("чтож начем...\n");
+	
+	randomizator(&bebra);
+
+	cout << bebra << endl;
+
+	if (bebra == 1 )
+	{
+		pizdec();
+	}
+	else if (bebra == 2)
+	{
+		printf("вам фартануло и масть у вас отличная!!!");
+	}
+
+	cin.get();
+
+
+
+
+
+}
